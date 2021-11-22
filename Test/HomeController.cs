e7d2a22293f90014702d94668e249850e11a650b
Ashish Mishra <ashishm306@gmail.com>
2021-11-22 12:29:47 +0530

    Add project files.

---
+++ Test/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [Route("api/Test")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        
+        [HttpGet]
+        [Route("/api/Test/Showgrp")]
+        public string[] ShowGRP()
+        {
+            return new string[] { "Aditya","Deepika","Ashish","Shubham"};
+        }
+    }
+}
